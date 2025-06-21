7deb934956aac4075e2190a0bc234ab1d8fd1b5b
Sandeep Raghunandhan <sandeep.raghu15@gmail.com>
2025-06-20 23:49:01 -0400

    Filter by words which it makes sense to image gen

--- AnkiImageGen.py
+++ AnkiImageGen.py
@@ -10,10 +10,10 @@ def generate_image(client, text, filename):
     # print(prompt)
     result = client.images.generate(
         model="gpt-image-1",
-        prompt="Generate an visual to help remember the given vocabulary word in a flashcard app like Anki: {text}. Do not include the vocab word or any text in the image. Do like anime or cartoon style and not photo realistic.".format(text=text),
+        prompt="Generate an visual to help remember the given vocabulary word or phrase in a flashcard app like Anki: {text}. Do not include the vocab word or any text in the image. Do like anime or cartoon style and not photo realistic.".format(text=text),
     )
 
-    print("Generated image for: " + text)
+    # print("Generated image for: " + text)
 
     image_base64 = result.data[0].b64_json
     image_bytes = base64.b64decode(image_base64)
@@ -22,6 +22,15 @@ def generate_image(client, text, filename):
     with open(filename, "wb") as f:
         f.write(image_bytes)
     return filename
+
+def check_phrase(client: OpenAI, frontText: str, backText: str) -> bool:
+    response = client.responses.create(
+        model="gpt-4.1",
+        input="Read this pairing of words {frontText} and {backText} and determine if an AI generated image would be useful to memorize the word in Anki. " \
+        "If a word sounds really similar in both languages, reply 'false'. Please reply 'true' or 'false' without any other explanation in all lowercase".format(frontText=frontText, backText=backText),
+    )
+    return response.output_text == "true"
+
 
 def main():
     deckname = sys.argv[1]
@@ -34,10 +43,13 @@ def main():
             frontText = result['fields']['Front']['value']
             backText = result['fields']['Back']['value']
             ## Just run it on single words and cards without images yet.
-            if "<img" not in frontText and len(frontText.split()) == 1:
+            if "<img" not in frontText and check_phrase(client, frontText, backText):
+                print("Adding image for: " + backText)
                 filename = generate_image(client=client, text=backText, filename=str(cardID)+".png")
                 path_to_file = Path(__file__).parent.as_posix() + "/" + filename
                 result = invoke("updateNoteFields", note={"id":cardID, "fields": {"Front":frontText, "Back":backText}, "picture": [{"filename":filename, "fields": ["Front"],"path":path_to_file}]})
+            else:
+                print("Skipping image for: " + backText)
         except Exception as e:
             print(e)
             continue
